ffe11fd1d9d154428e1935f9d63bbf55879b3769
A-rehman03 <abdurrehmannawaz8@gmail.com>
2024-07-21 01:08:06 +0500

    updated code

--- Server/utils/generateOTP.js
+++ Server/utils/generateOTP.js
@@ -1,7 +1,7 @@
 const otpGenerator = require('otp-generator')
 
 const generateOTP = () => {
-    const OTP = otpGenerator.generate(6, {
+    const OTP = otpGenerator.generate(6, {          
         upperCaseAlphabets: false,
         specialChars: false,
     }
